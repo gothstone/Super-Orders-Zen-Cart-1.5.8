9fcf4b38d483dcb0f3fc786f537499bb6379d4df
gothstone <55921619+gothstone@users.noreply.github.com>
2023-09-30 12:54:13 -0500

    1.5.8 version

---
+++ 2_Modified_Core_Files/YOUR_ADMIN/includes/languages/english/lang.invoice.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Scott C Wilson 2022 Jan 11 New in v1.5.8-alpha $
+*/
+
+$define = [
+    'ENTRY_CUSTOMER' => 'CUSTOMER:',
+    'ENTRY_PAYMENT_METHOD' => 'Payment Method:',
+    'ENTRY_SHIPPING' => 'Shipping:',
+    'TEXT_INFO_ATTRIBUTE_FREE' => '&nbsp;-&nbsp;FREE',
+/* Super Order defines */
+    'HEADER_INVOICE' => 'Invoice - Order #',
+    'HEADER_INVOICES' => 'Invoices',
+    'HEADER_TAX_ID' => 'Tax ID #',
+    'HEADER_PHONE' => 'Phone:',
+    'HEADER_FAX' => 'Fax:',
+    'HEADER_CUSTOMER_NOTES' => 'Order Notes:',
+    'HEADER_PO_NUMBER' => 'P.O. Number:',
+    'HEADER_PO_INVOICE_DATE' => 'Invoice Date:',
+    'HEADER_PO_TERMS' => 'Terms:',
+    'HEADER_PO_TERMS_LENGTH' => '30 Days',
+    'TABLE_HEADING_PRICE_NO_TAX' => 'Unit Price',
+    'TABLE_HEADING_TOTAL_NO_TAX' => 'Total',
+    'ENTRY_BILL_TO' => 'BILL TO:',
+    'ENTRY_PO_INFO' => 'P.O. DETAILS',
+    'ENTRY_NO_TAX' => 'Tax Exempt',
+    'ENTRY_PAYMENT_METHOD' => 'Payment Method:',
+    'ENTRY_AMOUNT_APPLIED_CUST' => 'Amount Applied:',
+    'ENTRY_BALANCE_DUE_CUST' => 'Balance Due:',
+    'ENTRY_AMOUNT_APPLIED_SHOP' => 'Amount Applied: (Default Store Currency)',
+    'ENTRY_BALANCE_DUE_SHOP' => 'Balance Due: (Default Store Currency)',
+];
+
+return $define;
+
+
+
