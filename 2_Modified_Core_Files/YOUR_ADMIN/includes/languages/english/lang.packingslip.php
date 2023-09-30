93bedcc47d5c2d7f4d0dbeb9e56c395ca0aa5a57
gothstone <55921619+gothstone@users.noreply.github.com>
2023-09-30 13:05:04 -0500

    1.5.8 ver
    replaces packingslip.php

---
+++ 2_Modified_Core_Files/YOUR_ADMIN/includes/languages/english/lang.packingslip.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Scott C Wilson 2022 Jan 11 New in v1.5.8-alpha $
+*/
+
+$define = [
+    'ENTRY_CUSTOMER' => 'CUSTOMER:',
+    'ENTRY_PAYMENT_METHOD' => 'Payment Method:',
+/* Super Order defines */
+    'HEADER_PACKINGSLIP' => 'Packing Slip - Order #',
+    'HEADER_PACKINGSLIPS' => 'Packing Slips',
+    'TEXT_PACKING_SLIP' => 'Packing Slip',
+    'TABLE_HEADING_IMAGE' => 'Image',
+    'TABLE_HEADING_QTY' => 'Qty',
+    'HEADER_CUSTOMER_NOTES' => 'Order Notes:',
+    'ENTRY_SHIPPING_METHOD' => 'Shipping Method:',
+    'ENTRY_PRODUCTS_INCL' => 'Product(s) included in this shipment',
+    'ENTRY_PRODUCTS_EXCL' => 'Product(s) will ship separately',
+];
+
+return $define;
